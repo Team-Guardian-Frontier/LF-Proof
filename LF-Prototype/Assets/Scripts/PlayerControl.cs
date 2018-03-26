e512f516a6d645e45e0903f23bdabeaa79e4a92a
Irvin Naylor <Inaylor003@gmail.com>
2018-03-26 10:33:19 -0400

    Basic 8-Directional Movement Created

---
+++ LF-Prototype/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float moveSpeed;
+    private Rigidbody2D myRigidbody;
+
+    private Vector3 moveInput;
+
+
+	
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Vector2.right * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector2.up * moveSpeed);
+        }
+
+
+    }
+}

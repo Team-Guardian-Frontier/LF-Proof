33d312865e5df5e6e694cf4ca8d0c727e4be8cdb
LAPTOP-IA658AG2\It's Dillon Zhong <pineapplegamedesign@gmail.com>
2018-04-06 19:17:41 -0400

    manual merge aiming

---
+++ LF-Prototype/Assets/Scripts/MousePos.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*This script is for the mouse position and getting the sprite (rectangle) to look at the cursor  
+ * 
+ */
+
+public class MousePos : MonoBehaviour {
+
+    //Sprite Stuff
+    public Sprite N, NE, E, SE, S, SW, W, NW;
+    public Camera mainCam;
+    public float mAngle;
+
+    public float xdisty;
+    public float ydisty;
+
+    private Vector3 gps;
+    
+    // Use this for initializing
+    void Start () {
+        gps = new Vector3(0, 0, 0);
+	}
+
+    // Update is called once per frame
+    void Update() {
+        /*
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(
+        mousePosition.x - transform.position.x,
+        mousePosition.y - transform.position.y
+        );
+
+        transform.up = direction;
+        */
+        MouseAngle();
+        MouseRotation();
+        
+    }
+    
+    //Utility methods
+    
+    // Calculate angle from character to mouse, for purposes of rotation or aim
+    void MouseAngle()
+    {
+        //calculate manually.
+        //Mouse is in pixel Loc(ScreenPoint) and char is on World Point.
+        Vector3 charPos = mainCam.WorldToScreenPoint(transform.position);
+        Vector3 mousePos = Input.mousePosition;
+        float xdist = charPos.x - mousePos.x;
+        float ydist = charPos.y - mousePos.y;
+
+        mAngle = -Mathf.Atan2(ydist, xdist) * Mathf.Rad2Deg;
+
+        //Debug
+        xdisty = charPos.x;
+        ydisty = mousePos.x;
+
+
+    }
+
+    //Changes sprite according to mouse angle
+    void MouseRotation()
+    {
+        //call mouse angle
+        float angle = mAngle;
+
+        //Switch case to change sprite
+        if (angle <= 22.5 && angle > -22.5)
+        {
+            //west
+            this.GetComponent<SpriteRenderer>().sprite = W;
+        }
+        else if (angle <= 67.5 && angle > 22.5)
+        {
+            //Northwest
+            this.GetComponent<SpriteRenderer>().sprite = NW;
+        }
+        else if (angle <= 112.5 && angle > 67.5)
+        {
+            //North
+            this.GetComponent<SpriteRenderer>().sprite = N;
+        }
+        else if (angle <= 157.5 && angle > 112.5)
+        {
+            //Northeast
+            this.GetComponent<SpriteRenderer>().sprite = NE;
+        }
+        //negative
+        else if (angle <= -22.5 && angle > -67.5)
+        {
+            //Southwest
+            this.GetComponent<SpriteRenderer>().sprite = SW;
+        }
+        else if (angle <= -67.5 && angle > -112.5)
+        {
+            //South
+            this.GetComponent<SpriteRenderer>().sprite = S;
+        }
+        else if (angle <= -112.5 && angle > -157.5)
+        {
+            //Southeast
+            this.GetComponent<SpriteRenderer>().sprite = SE;
+        }
+        else if (angle <= -157.5 || angle >157.5)
+        {
+            //East
+            this.GetComponent<SpriteRenderer>().sprite = E;
+        }
+
+
+    }
+
+}
---
+++ LF-Prototype/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script is for controlling player movement on an 8-directional movement
+//It also serves as Sprite changing
+
+public class PlayerControl : MonoBehaviour {
+
+    public float moveSpeed;
+    private Rigidbody2D myRigidbody;
+
+    private float CDIAG = (Mathf.Sqrt(2)/2);
+
+
+
+    void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * moveSpeed * CDIAG);
+            transform.Translate(Vector2.up * moveSpeed * CDIAG);
+        }
+        else
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.left * moveSpeed * CDIAG);
+            transform.Translate(Vector2.down * moveSpeed * CDIAG);
+        }
+        else
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.right * moveSpeed * CDIAG);
+            transform.Translate(Vector2.down * moveSpeed * CDIAG);
+        }
+        else
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.left * moveSpeed * CDIAG);
+            transform.Translate(Vector2.up * moveSpeed * CDIAG);
+        }
+        else
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * moveSpeed);
+
+        }
+        else
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Vector2.right * moveSpeed);
+            
+        }
+        else
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * moveSpeed);
+            
+        }
+        else
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector2.up * moveSpeed);
+            
+        }
+        
+        
+
+    }
+}

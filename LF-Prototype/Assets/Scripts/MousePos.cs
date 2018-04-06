628b5df3dccad1061827af003718d59e45bd3b83
LAPTOP-IA658AG2\It's Dillon Zhong <pineapplegamedesign@gmail.com>
2018-04-06 19:06:24 -0400

    Manual Merging

--- LF-Prototype/Assets/Scripts/MousePos.cs
+++ LF-Prototype/Assets/Scripts/MousePos.cs
@@ -6,23 +6,5 @@ using UnityEngine;
 
 public class MousePos : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 
-    // Update is called once per frame
-    void Update() {
-        /*
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-        Vector2 direction = new Vector2(
-        mousePosition.x - transform.position.x,
-        mousePosition.y - transform.position.y
-        );
-
-        transform.up = direction;
-        */
-    }
 }
--- LF-Prototype/Assets/Scripts/PlayerControl.cs
+++ LF-Prototype/Assets/Scripts/PlayerControl.cs
@@ -10,10 +10,10 @@ public class PlayerControl : MonoBehaviour {
     public float moveSpeed;
     private Rigidbody2D myRigidbody;
 
-    private Vector3 moveInput;
+
 
     //Sprite Stuff
-    public Sprite N, NE, E, SE, S, SW, W, NW;
+    
 
 
 
@@ -23,50 +23,7 @@ public class PlayerControl : MonoBehaviour {
 	
 	
 	void Update () {
-		if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector2.right * moveSpeed);
-            this.GetComponent<SpriteRenderer>().sprite = E;
-        }
-
-        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))
-        {
-            this.GetComponent<SpriteRenderer>().sprite = SE;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(-Vector2.right * moveSpeed);
-            this.GetComponent<SpriteRenderer>().sprite = W;
-        }
-
-        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S))
-        {
-            this.GetComponent<SpriteRenderer>().sprite = SW;
-        }
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector2.up * moveSpeed);
-            this.GetComponent<SpriteRenderer>().sprite = N;
-        }
-
-        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
-        {
-            this.GetComponent<SpriteRenderer>().sprite = NW;
-        }
-
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(-Vector2.up * moveSpeed);
-            this.GetComponent<SpriteRenderer>().sprite = S;
-        }
-
-        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
-        {
-            this.GetComponent<SpriteRenderer>().sprite = NE;
-        }
+		
 
 
     }

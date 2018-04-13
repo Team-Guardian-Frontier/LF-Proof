0f69f70ea8c91c30f513cae78690f168e4bac087
Irvin Naylor <Inaylor003@gmail.com>
2018-04-13 19:13:41 -0400

    Shooting placeholder is done (Doesn't shoot yet)

---
+++ LF-Prototype/Assets/Scripts/WeaponScripts/Gun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public float fireRate = 3; //dependant on gun
+    public float damage = 8; //default damage value
+    float TTF = 0; //Time until Fire
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && Time.time > TTF)
+        {
+            TTF = Time.time + 1 / fireRate;
+            Shoot();
+        }
+        
+
+    }
+
+    void Shoot()
+    {
+        //Shooting placeholder
+        Debug.Log("Shooting");
+    }
+
+}

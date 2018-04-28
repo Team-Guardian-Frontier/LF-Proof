f04ac00e47c9dec3cc24d2551a6b540538ec6861
LAPTOP-IA658AG2\It's Dillon Zhong <pineapplegamedesign@gmail.com>
2018-04-28 13:45:00 -0400

    Added controller support

--- LF-Prototype/Assets/Scripts/MousePos.cs
+++ LF-Prototype/Assets/Scripts/MousePos.cs
@@ -10,87 +10,79 @@ public class MousePos : MonoBehaviour {
     // Sprite Stuff
     public Sprite N, NE, E, SE, S, SW, W, NW;
     public Camera mainCam;
-    public float mAngle;
+    public float RAngle;
 
     // Use this for initializing
     void Start () {
 
 	}
 
-    private float hAxis;
-    private float vAxis;
     private float aimX;
     private float aimY;
+    
+    //part of Toggle
+    //private float togDead = .5f;
 
-    void ControllerCheck() {
-        float ltaxis = Input.GetAxis("XboxLeftTrigger");
-        float rtaxis = Input.GetAxis("XboxRightTrigger");
-
-        bool xbox_a = Input.GetButton("XboxA");
-
-        hAxis = Input.GetAxis("Horizontal");
-        vAxis = Input.GetAxis("Vertical");
-
-        aimX = Input.GetAxis("AimX");
-        aimY = Input.GetAxis("AimY");
-    }
 
     // Update is called once per frame
     void Update() {
         // Camera Rig Movement Control
 
-        ControllerCheck();
-
-        MouseAngle();
+        aimX = Input.GetAxis("AimX");
+        aimY = Input.GetAxis("AimY");
+        /*
+         * Toggle, Saves last direction. Odd issue registering.
+        float aimDelta = (togDead - Mathf.Abs(aimX)) + (togDead - Mathf.Abs(aimY));
+         * 
+         * if (aimDelta < 0)
+            StickAngle();
+        */
+        StickAngle();
         MouseRotation();
-    }
+            }
     
     //Utility methods
     
     // Calculate angle from character to mouse, for purposes of rotation or aim
-    void MouseAngle() {
-        // calculate manually.
-        // Mouse is in pixel Loc(ScreenPoint) and char is on World Point.
-        Vector3 charPos = mainCam.WorldToScreenPoint(transform.position);
-        Vector3 mousePos = Input.mousePosition;
-        float xdist = charPos.x - mousePos.x;
-        float ydist = charPos.y - mousePos.y;
+    void StickAngle() {
 
-        mAngle = -Mathf.Atan2(ydist, xdist) * Mathf.Rad2Deg;
+        //calculate angle from controller
+        RAngle = -(Mathf.Atan2(aimY, aimX) * Mathf.Rad2Deg);
+
     }
 
     // Changes sprite according to mouse angle
     void MouseRotation() {
         // call mouse angle
-        float angle = mAngle;
+        float angle = RAngle;
 
         // Switch case to change sprite
         if (angle <= 22.5 && angle > -22.5) {
             // west
-            this.GetComponent<SpriteRenderer>().sprite = W;
+            this.GetComponent<SpriteRenderer>().sprite = E;
         } else if (angle <= 67.5 && angle > 22.5) {
             // Northwest
-            this.GetComponent<SpriteRenderer>().sprite = NW;
+            this.GetComponent<SpriteRenderer>().sprite = NE;
         } else if (angle <= 112.5 && angle > 67.5) {
             // North
             this.GetComponent<SpriteRenderer>().sprite = N;
         } else if (angle <= 157.5 && angle > 112.5) {
             // Northeast
-            this.GetComponent<SpriteRenderer>().sprite = NE;
+            this.GetComponent<SpriteRenderer>().sprite = NW;
         }
           // negative
           else if (angle <= -22.5 && angle > -67.5) {
             // Southwest
-            this.GetComponent<SpriteRenderer>().sprite = SW;
+            this.GetComponent<SpriteRenderer>().sprite = SE;
         } else if (angle <= -67.5 && angle > -112.5) {
             // South
             this.GetComponent<SpriteRenderer>().sprite = S;
         } else if (angle <= -112.5 && angle > -157.5) {
             // Southeast
-            this.GetComponent<SpriteRenderer>().sprite = SE;
+            this.GetComponent<SpriteRenderer>().sprite = SW;
         } else if (angle <= -157.5 || angle > 157.5) {
             // East
-            this.GetComponent<SpriteRenderer>().sprite = E;
+            this.GetComponent<SpriteRenderer>().sprite = W;
         }
     }
 }

a2bfa2d06ac1201ce53017a0fc9daff1d0fe229e
Irvin Naylor <Inaylor003@gmail.com>
2018-04-28 20:29:58 -0400

    Meems

---
+++ LF-Prototype/Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour {
+
+    private Food food;
+    private int foodNumber;
+    private Food.FoodType spawnType;
+    private int foodCount = 0;
+    private int maxFood = 10; // may change for "food frenzy"
+    private bool spawnAvailable = true; // determines whether food can spawn
+    private Vector2 spawnLocation;
+    private float spawnTimer = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+        Spawner();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    // Spawns food at random locations at a time interval based on the "timer" variable
+    // An int timer spawns the food in rows and columns, great for specifying static spawn points (tends to spawn in the same spots)
+    // A float timer makes the location completely random
+    void Spawner()
+    {
+        while (foodCount < maxFood)
+        {
+            foodNumber = 0; // Set to Fruit for debugging
+                            //foodNumber = Random.Range(0, 4);                                                  // Randomizes the FoodType
+            spawnType = (Food.FoodType)foodNumber;                                              // Sets the FoodType using the random number
+            spawnLocation = new Vector2(Random.Range(-4.8f, 4.8f), Random.Range(-2.5f, 2.5f));  // Sets the random location of food spawn within a certain area
+
+            // check an area around the food to see if another food is nearby, if so then move the spawn location to another spot
+            Collider2D foodInArea = Physics2D.OverlapCircle(spawnLocation, 1.0f);
+            while (foodInArea != null)
+            {
+                spawnLocation = new Vector2(Random.Range(-3.0f, 3.0f), Random.Range(-1.5f, 1.5f)); // checks a smaller area if the first location is too close
+                foodInArea = null; // reset array
+                foodInArea = Physics2D.OverlapCircle(spawnLocation, 1f); // check again
+            }
+            // spawn the food
+            food = new Food(spawnType, spawnLocation);
+            foodCount += 1;
+            Debug.Log("Created food: '" + food.foodType + "' at " + Time.time);
+        }
+    }
+}
---
+++ LF-Prototype/Assets/Scripts/FruitHandler.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class FruitHandler : MonoBehaviour {
+
+    //Player Stats
+    private float health;
+    private float veggies;
+    private float protein;
+    private float grain;
+
+    //fruit object space
+
+
+	// Use this for initialization
+	void Start () {
+        //instantiate Stats
+        health = 100;
+        veggies = 0;
+        protein = 0;
+        grain = 0;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        /*if Game Tag,
+        Read type,
+        set object to hold.
+
+        Need to have a variable to hold the specific object.
+
+        Shoot object, on button.
+
+        */
+
+    }
+
+    /*  Guide to food interaction:
+     *  Fruit will have these states.
+     *      - Held
+     *          maybe decrease the fruit sprite size, and have it follow based on character's position.
+     *      -shot
+     *          passed in angle from player, and move at a consistent speed.
+     *              set a specific variable to read if hit something like that.
+     *  Player will have these actions:
+     *      - Eat
+     *          delete fruit, Read type, and add health and food type
+     *      - Shoot
+     *          put fruit in shot state, pass the shooting angle.
+     *      
+     *      - the collision
+     *          Player will hold the object in a field. when colliding, check tag, 
+     *              and place collider's object in field
+     *              - Hit
+     *          If hit with an object in shot, then read in damage and food type.
+     *              if food type ever goes over, apply a flat damage. this will decrease healing, and add damage.
+     * 
+     */
+}

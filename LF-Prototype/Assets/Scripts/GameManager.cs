5bdb56238ced89c98601622d7f935d0260977405
Irvin Naylor <Inaylor003@gmail.com>
2018-08-28 15:22:17 -0400

    Tilemap system is up, floor and wall done

--- LF-Prototype/Assets/Scripts/GameManager.cs
+++ LF-Prototype/Assets/Scripts/GameManager.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
     private static GameManager instance = null;
 
+    public BoardManager boardScript;
+
+
+
     //Awake is always called before any Start functions
     void Awake() {
-        if (instance == null)
+
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+        /*if (instance == null)
             instance = this;
         else if (instance != this)
             Destroy(gameObject);
         
         DontDestroyOnLoad(gameObject);
+        */
+    }
+
+    void InitGame()
+    {
+        Debug.Log("I made a boardmanager");
     }
 
 	void Start() {

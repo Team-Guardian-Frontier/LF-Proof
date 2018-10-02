790930c53ec8bece5ff5f72636b0686c0b6a2e17
LAPTOP-IA658AG2\It's Dillon Zhong <pineapplegamedesign@gmail.com>
2018-10-02 01:19:49 -0400

    Working Audio, v1

---
+++ LF-Prototype/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,63 @@
+﻿
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+/*
+ * source tutorial
+ * https://www.youtube.com/watch?v=6OT43pvUyfY
+ */
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+    //instance checker
+    public static AudioManager instance;
+
+	// Use this for initialization
+	void Awake () {
+
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            //Initialize each sound with an AudioSource component.
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+	}
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, Sound => Sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound" + name + " not Found!");
+            return;
+        }
+        s.source.Play();
+    }
+
+    void Start()
+    {
+        Play("Theme1");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}

8635b84a0c979720b2817f4111efb4b20459c561
LAPTOP-IA658AG2\It's Dillon Zhong <pineapplegamedesign@gmail.com>
2018-10-16 00:08:04 -0400

    Pause Menu v2! Sounds, UI Visuals

--- LF-Prototype/Assets/Scripts/AudioManager.cs
+++ LF-Prototype/Assets/Scripts/AudioManager.cs
@@ -40,6 +40,21 @@ public class AudioManager : MonoBehaviour {
 
 	}
 
+    public Sound Find(string name)
+    {
+        //array search
+        Sound s = Array.Find(sounds, Sound => Sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound" + name + " not Found!");
+            return null;
+        }
+
+
+        return (s);
+
+    }
+
     public void Play(string name)
     {
         Sound s = Array.Find(sounds, Sound => Sound.name == name);
@@ -62,6 +77,7 @@ public class AudioManager : MonoBehaviour {
         s.source.Stop();
     }
 
+
     public Sound PlayClone(string name)
     {
             Sound s = Array.Find(sounds, Sound => Sound.name == name);
@@ -82,6 +98,35 @@ public class AudioManager : MonoBehaviour {
             
         //will need to manually stop by the source.
             
+    }
+
+    public void PauseAudio()
+    {
+        //pause all audio except the pause sound. name is hardcoded.
+        foreach (Sound s in sounds)
+        {
+            s.source.Pause();
+        }
+
+        //hardcoded, just for speed
+        Sound s2 = Array.Find(sounds, Sound => Sound.name == "PauseSound");
+        Sound s3 = Array.Find(sounds, Sound => Sound.name == "ResumeSound");
+        s3.source.UnPause();
+        s2.source.Play();
+        //might have some funky interactions with resume sound.
+    }
+
+    public void UnPauseAudio()
+    {
+        //pause all audio except the pause sound. name is hardcoded.
+        foreach (Sound s in sounds)
+        {
+            s.source.UnPause();
+        }
+
+        //hardcoded for ease
+        Sound s2 = Array.Find(sounds, Sound => Sound.name == "ResumeSound");
+        s2.source.Play();
     }
 
     private void SourceInstantiate(Sound s)
--- LF-Prototype/Assets/Scripts/Restart.cs
+++ LF-Prototype/Assets/Scripts/Restart.cs
@@ -5,46 +5,75 @@ using UnityEngine.SceneManagement;
 
 /*
  * Author: Irvin Naylor
- * Last Change: 7/13/18
- * What script does: Contains way to restart game upon pressing ESC
- * Notes: - Temporary for now
- *        - Attached to GameManager object
+ * Last Change: 10/15/18 - Dillon
+ * What script does: Contains script for activating Pause Menu, and Restarting Game.
+ * Notes: - Attached to Event System. (Good for management within scene)
+ *        - Pause all sounds except pause menu?
+ * 
  */
 
 public class Restart : MonoBehaviour {
 
-    private bool isPaused;
+    public static bool isPaused = false;
+    public GameObject PauseMenuUI;
 
-    private void Awake()
+
+
+    private void Start()
     {
+       
+    }
+
+    void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.Escape)) //press escape at any time
+        {
+
+
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.BackQuote))
+        {
+            //Reset the Game
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+            isPaused = false;
+        }
+    }
+
+    //Activate Pause
+    void Pause()
+    {
+        //Make PauseMenu Active
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+
+        //Pause Sound
+        FindObjectOfType<AudioManager>().PauseAudio();
+
+
+        isPaused = true;
+
+    }
+
+    void Resume()
+    {
+        //Deactivate PauseMenu
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+
+        //Resume Sound
+        FindObjectOfType<AudioManager>().UnPauseAudio();
+
         isPaused = false;
 
     }
-    // Update is called once per frame
-    void Update () {
-
-   
-		if( Input.GetKeyDown(KeyCode.Escape)) //press escape at any time
-        {
-            isPaused = !isPaused;
-        }
-        
-        if (isPaused)
-        {
-            Time.timeScale = 0;
-
-            Debug.Log("I pause");
-
-            if (Input.GetKeyDown(KeyCode.BackQuote))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name); //scene resets
-                isPaused = false;
-            }
-        }
-        else
-        {
-            Time.timeScale = 1;
-        }
-        
-	}
 }

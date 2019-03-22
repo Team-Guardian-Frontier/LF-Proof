86a754d546812dc33a5fe0a9dfe3a6b6e9266fc2
Justin Carrasquillo <justin.carrasquillo2000@gmail.com>
2019-03-22 17:40:01 -0400

    Fixing commit

---
+++ LF-Prototype/Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Enables the play button to move to the next scene
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Enables the quit button to exit out of the game
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+
+}

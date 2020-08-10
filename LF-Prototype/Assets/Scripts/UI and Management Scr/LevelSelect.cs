4a705f1fb4382be92c032e4cbc707db287d40034
Justin Carrasquillo <justin.carrasquillo2000@gmail.com>
2020-08-10 13:37:25 -0400

    add a level select menu

---
+++ LF-Prototype/Assets/Scripts/UI and Management Scr/LevelSelect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
